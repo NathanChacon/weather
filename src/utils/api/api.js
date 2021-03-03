05c5a7f57138671bba8a37a921f4ace45f522593
Nathan Chacon <nathanchacon321@gmail.com>
2021-03-03 19:51:21 -0300

    Add my own cors

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/",
+  baseURL: "https://cors-anywhere-nathan-chacon.herokuapp.com/https://www.metaweather.com/api/",
   responseType: "json"
 });
